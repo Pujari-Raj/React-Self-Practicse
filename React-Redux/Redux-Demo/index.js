c5e1d0553cc058cf52c67b29ee38a7f881c80f47
Pujari-Raj <basvarajpujari607@gmail.com>
2023-06-16 23:35:11 +0530

    started redux witj multiple reducer demo

--- React-Redux/Redux-Demo/index.js
+++ React-Redux/Redux-Demo/index.js
@@ -1,30 +1,82 @@
+const redux = require("redux");
+const createStore = redux.createStore;
 
-const CAKE_ORDERED = 'CAKE_ORDERED'
+//
+const bindActionCreators = redux.bindActionCreators;
 
-// state of application (assume we have in-total 12 cakes)
+// actions
+const CAKE_ORDERED = 'CAKE_ORDERED';
+const CAKE_RESTOCKED = 'CAKE_RESTOCKED';
+
+
+// state of application (assume we have in-total 12 cakes) Step-2 Declare initialState
 const initialState = {
-    numOfCakes : 10,
+    numOfCakes: 10,
 }
 
-// created action
+// created action Step-4 Define Action & Action Creators
 function orderCake() {
-    return{
-      type :  'CAKE_ORDERED', 
-      quantity :  1
-      }    
+    return {
+        type: 'CAKE_ORDERED',
+        // quantity: 1
+        payload : 1,
+    }
 }
 
-// Reducer (previousState, aaction) = newState
+// function for restocking the cake
+function restokeCake(qty = 1) {
+    return {
+        type: 'CAKE_RESTOCKED',
+        // quantity: qty,
+        payload : qty,
+        /* In Redux payload is the convention used for any additional information we want to send */
+    }
+}
+
+
+// Reducer (previousState, aaction) = newState Step-3 Create Reducer
 
 const reducer = (state = initialState, action) => {
     switch (action.type) {
         case CAKE_ORDERED:
-            return{
+            return {
                 ...state,
                 numOfCakes: state.numOfCakes - 1,
             }
-    
+
+        case CAKE_RESTOCKED:
+            return {
+                ...state,
+                numOfCakes: state.numOfCakes + action.payload,
+            }
+
         default:
             return state;
     }
 }
+
+//step-1 Creating a Store
+const store = createStore(reducer);
+console.log('initialState-', store.getState());
+
+//Step-5 Subscribe to store
+const unsubscribe = store.subscribe(() => {
+    console.log('Update State-', store.getState());
+})
+
+//Step- subscribe to actions to update the store  
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(orderCake());
+// store.dispatch(restokeCake(4));
+ 
+// using bindActionCreators (bindActionCreators is convetion for calling actions in redux)
+const actions = bindActionCreators({orderCake, restokeCake}, store.dispatch);
+actions.orderCake();
+actions.orderCake();
+actions.orderCake();
+actions.restokeCake(5);
+
+// & finally unsubscribe to the changes
+unsubscribe();
+
