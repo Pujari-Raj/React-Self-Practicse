fc20431d252174bdc4cbe87be39be13b3119f8f2
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-08-08 23:14:32 +0530

    redux toolkit demo-2 started

---
+++ Redux-Toolkit/redux-toolkit-2/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import PostLists from "./features/posts/PostLists";
+
+function App() {
+  return (
+    <div className="App">
+      <PostLists/>
+    </div>
+  );
+}
+
+export default App;
---
+++ Redux-Toolkit/redux-toolkit-2/src/features/posts/postSlice.js
@@ -0,0 +1,31 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+// creating initialState and slice for posts (this are all the posts which will be shown on webpage)
+const initialState = [
+    {id: '1', title: 'Learning Redux', desc: "How you doing!!, Ahhh"},
+    {id: '2', title: 'Learning Redux Toolkit', desc: "Joey Doesn't share Food"},
+]
+
+const postSlice = createSlice({
+    name: 'posts',
+    initialState,
+    // 
+    reducers: {
+        // functn for adding post , from form
+        /* postAdded func automatically generate the action creator fucn with same name */
+        postAdded(state, action){
+            /* state.push(...) seems like it's mutating a state but behind the scenes
+                it's not mutating the state , bcz immer-js creates the new state underneath 
+                and it only in createSlice functn , if you try writing the same code 
+                anywhere else in application it'll not work.
+            */
+            state.push(action.payload);
+        }
+    }
+})
+
+export const selectAllPosts = (state) => state.posts;
+
+export const { postAdded } = postSlice.actions;
+
+export default postSlice.reducer;
