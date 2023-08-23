e62ccd464da277903622e6349f1e3145376b1cd3
Pujari Basvaraj <basvarajpujari607@gmail.com>
2023-08-23 23:25:26 +0530

    Redux App Structure concept

---
+++ Redux-Toolkit/redux-toolkit-2/src/features/posts/ReactionButtons.js
@@ -0,0 +1,34 @@
+import { useDispatch } from "react-redux";
+import { reactionAdded } from "./postSlice";
+import React from "react";
+
+const reactionEmoji = {
+  thumbsUp: "👍",
+  wow: " 😵",
+  heart: "❤️",
+  rocket: "🚀",
+  clap: "🎬",
+};
+
+
+
+const ReactionButtons = ({ post }) => {
+    console.log('reactionButton clicked');
+  const dispacth = useDispatch()
+
+  const reactionButtons = Object.entries(reactionEmoji).map(([name, emoji]) => {
+    return(
+        <button
+        key={name}
+        className="reactionButton"
+        onClick={() => dispacth(reactionAdded({ postId: post.id, reaction: name }))}
+        >
+            {emoji} {post.reactions[name]}
+        </button>
+    )
+  })
+
+  return <div>{reactionButtons}</div>
+}
+
+export default ReactionButtons;
--- Redux-Toolkit/redux-toolkit-2/src/features/posts/postSlice.js
+++ Redux-Toolkit/redux-toolkit-2/src/features/posts/postSlice.js
@@ -6,10 +6,24 @@ const initialState = [
     { id: '1', 
     title: 'Learning Redux ', content: "How you doing!!, Ahhh",
     date : sub(new Date(), { minutes: 10}).toISOString(),
+    reactions: {
+        thumbsUp: 0,
+        wow: 0,
+        heart: 0,
+        rocket: 0,
+        clap: 0
+    }
 },
     { id: '2', 
     title: 'Learning Redux  Toolkit', content: "Joey Doesn't share Food",
     date : sub(new Date(), { minutes: 5}).toISOString(),
+    reactions: {
+        thumbsUp: 0,
+        wow: 0,
+        heart: 0,
+        rocket: 0,
+        clap: 0
+    }
 },
 ]
 
@@ -20,6 +34,7 @@ const postSlice = createSlice({
     reducers: {
         // functn for adding post , from form
         /* postAdded func automatically generate the action creator fucn with same name */
+        /*The postAdded reducer function is defined as an object with two properties: reducer and prepare. */
         postAdded: {
             reducer(state, action) {
                 /* state.push(...) seems like it's mutating a state but behind the scenes
@@ -29,7 +44,12 @@ const postSlice = createSlice({
                 */
                 state.push(action.payload);
             },
-            //
+        /*The prepare property is a function that generates the action creator 
+        function for the postAdded action. It takes the title, content, and userId 
+        as arguments, and returns an object with a payload property. 
+        The payload object contains properties such as id, title, content, date, 
+        userId, and reactions, which are used to create a new post.
+         */
             prepare(title, content, userId) {
                 return {
                     payload: {
@@ -37,9 +57,24 @@ const postSlice = createSlice({
                         title,
                         content,
                         date: new Date().toISOString(),
-                        userId
+                        userId,
+                        reactions: {
+                            thumbsUp: 0,
+                            wow: 0,
+                            heart: 0,
+                            rocket: 0,
+                            clap: 0,
+                        }
                     }
                 }
+            }
+        },
+        // handles an action for adding a reaction to a post.
+        reactionAdded(state, action) {
+            const { postId, reaction} = action.payload
+            const existingPost = state.find(post => post.id === postId);
+            if (existingPost) {
+                existingPost.reactions[reaction]++;
             }
         }
     }
@@ -47,6 +82,6 @@ const postSlice = createSlice({
 
 export const selectAllPosts = (state) => state.posts;
 
-export const { postAdded } = postSlice.actions;
+export const { postAdded, reactionAdded } = postSlice.actions;
 
 export default postSlice.reducer;
